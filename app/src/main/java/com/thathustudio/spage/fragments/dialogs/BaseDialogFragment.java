70fbd118c0d9ff81c15571a597387cf7541bffa1
Vũ Hoàng Phúc <ryanvu644@gmail.com>
2016-12-12 00:09:42 +0700

    - Optimize imports - Sample code for TempActivity - Add package fragments.dialogs

--- app/src/main/java/com/thathustudio/spage/fragments/dialogs/BaseDialogFragment.java
+++ app/src/main/java/com/thathustudio/spage/fragments/dialogs/BaseDialogFragment.java
@@ -1,15 +1,11 @@
-package com.thathustudio.spage.fragments;
+package com.thathustudio.spage.fragments.dialogs;
 
-
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 
 import com.squareup.leakcanary.RefWatcher;
 import com.thathustudio.spage.CustomApplication;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public abstract class BaseFragment extends Fragment {
+public class BaseDialogFragment extends DialogFragment {
 
     @Override
     public void onDestroy() {
--- app/src/main/java/com/thathustudio/spage/temp/TempActivity.java
+++ app/src/main/java/com/thathustudio/spage/temp/TempActivity.java
@@ -1,9 +1,11 @@
 package com.thathustudio.spage.temp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.thathustudio.spage.R;
+import com.thathustudio.spage.activities.LoginActivity;
 
 public class TempActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+
+        // Redirect or show fragment
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        // -------------------------
+
+        finish();
     }
 }
