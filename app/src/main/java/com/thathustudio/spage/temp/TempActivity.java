ec7ccc908a174eac02d75075664dfccc0cf30a68
Vũ Hoàng Phúc <ryanvu644@gmail.com>
2017-01-02 13:51:33 +0700

    - Layout ResultActivity - Custom ArcProgressBar of third party library and apply it to ResultActivity - Change TempActivity's behavior - Remove cardview from gradle

--- app/src/main/java/com/thathustudio/spage/temp/TempActivity.java
+++ app/src/main/java/com/thathustudio/spage/temp/TempActivity.java
@@ -1,10 +1,13 @@
 package com.thathustudio.spage.temp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.thathustudio.spage.R;
-import com.thathustudio.spage.fragments.ExercisesFragment;
+import com.thathustudio.spage.activities.ResultActivity;
+
+import java.util.Random;
 
 public class TempActivity extends AppCompatActivity {
 
@@ -16,11 +19,20 @@ public class TempActivity extends AppCompatActivity {
         // Redirect or show fragment
 
         // - Redirect
-        /*startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        finish();*/
+        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+        Bundle bundle = new Bundle();
+        Random random = new Random(System.currentTimeMillis());
+        boolean[] results = new boolean[40];
+        for (int i = 0, len = results.length; i < len; i++) {
+            results[i] = random.nextBoolean();
+        }
+        bundle.putBooleanArray(ResultActivity.RESULT, results);
+        intent.putExtras(bundle);
+        startActivity(intent);
+        finish();
 
         // - Show fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.activity_temp, ExercisesFragment.newInstance()).commitAllowingStateLoss();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.activity_temp, ExercisesFragment.newInstance()).commitAllowingStateLoss();
 
         // -------------------------
     }
