669e7c0426e6e4fc34e45aaa5339f120b4e388e2
Blue <blubluescarlet@gmail.com>
2017-01-18 11:00:24 +0700

    init animation

--- app/src/main/java/com/thathustudio/spage/utils/RankRecyclerViewAdapter.java
+++ app/src/main/java/com/thathustudio/spage/utils/RankRecyclerViewAdapter.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.thathustudio.spage.R;
 import com.thathustudio.spage.model.Result;
 
@@ -14,48 +16,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class RankRecyclerViewAdapter extends RecyclerView.Adapter<RankRecyclerViewAdapter.ResultViewHolder> {
+public class RankRecyclerViewAdapter extends RecyclerView.Adapter<RankRecyclerViewAdapter.ResultViewHolder>
+{
     private final List<Result> results;
     private final Locale locale;
 
-    public RankRecyclerViewAdapter() {
+    public RankRecyclerViewAdapter()
+    {
         results = new ArrayList<>();
         locale = Locale.getDefault();
     }
 
-    public void replaceResults(List<Result> results) {
+    public void replaceResults(List<Result> results)
+    {
         this.results.clear();
         this.results.addAll(results);
         notifyDataSetChanged();
     }
 
     @Override
-    public ResultViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ResultViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rank, parent, false);
         return new ResultViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ResultViewHolder holder, int position) {
+    public void onBindViewHolder(ResultViewHolder holder, int position)
+    {
         holder.result = results.get(position);
         holder.textViewUserName.setText(holder.result.getUserName());
         holder.textViewScore.setText(String.format(locale, "%1$.2f", 1.0 * holder.result.getScore() / 100));
     }
 
     @Override
-    public int getItemCount() {
+    public int getItemCount()
+    {
         return results.size();
     }
 
-    public static class ResultViewHolder extends RecyclerView.ViewHolder {
+    public static class ResultViewHolder extends RecyclerView.ViewHolder
+    {
         public Result result;
         public final TextView textViewUserName;
         public final TextView textViewScore;
 
-        public ResultViewHolder(View itemView) {
+        public ResultViewHolder(View itemView)
+        {
             super(itemView);
             textViewUserName = (TextView) itemView.findViewById(R.id.txtV_rankUserName);
             textViewScore = (TextView) itemView.findViewById(R.id.txtV_rankScore);
+
+            YoYo.with(Techniques.SlideInLeft).duration(2000).playOn(textViewUserName);
+            YoYo.with(Techniques.SlideInLeft).duration(2000).playOn(textViewScore);
         }
     }
 }
